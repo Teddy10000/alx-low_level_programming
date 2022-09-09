cc4425c1ce915ef49689074f5b90474ee4c5ed8e
Teddy10000 <akrongkofi@gmail.com>
2022-09-08 17:44:34 -0700

    Fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
+#include <stdio.h>
 
-# include <stdio.h>
+
 
 /**
  *
@@ -10,9 +11,13 @@
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
