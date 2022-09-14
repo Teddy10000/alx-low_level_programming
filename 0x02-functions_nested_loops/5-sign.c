33696cb1e0dfc4c3096fb82c1836025653aed62d
Teddy10000 <akrongkofi@gmail.com>
2022-09-13 17:13:37 -0700

    Done to 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,54 @@
+# include "main.h"
+#include <unistd.h>
+
+/**
+ *  * print_sign - check numers
+ *
+ *   * @n : number to check
+ *
+ *    * Return:0 or 1
+ *
+ */
+
+int print_sign(int n)
+{
+
+	int check;
+
+
+
+	if (n > 0)
+
+	{
+
+		check = 1;
+
+		_putchar('+');
+
+	}
+
+	else if (n == 0)
+
+	{
+
+		check = 0;
+
+		_putchar('0');
+
+	}
+
+	else
+
+	{
+
+		check = -1;
+
+		_putchar('-');
+
+	}
+
+
+
+	return (check);
+
+}
