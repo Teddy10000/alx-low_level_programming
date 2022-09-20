60aac523eb9bb94b2d6bf1e7670c762f480137f8
Teddy10000 <akrongkofi@gmail.com>
2022-09-19 18:06:29 -0700

    ALL the assignemnt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+/**
+ *  * swap_int - swap variable values
+ *
+ *   * @a: pointer 1
+ *
+ *    * @b: pointer 2
+ *
+ *     * Return: void
+ *
+ */
+
+
+
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+
+	*b = *a - *b;
+
+	*a = *a - *b;
+}
