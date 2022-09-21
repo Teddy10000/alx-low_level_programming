3d9d0ebee446bbec0654f2fed16803e5db2198b5
Teddy10000 <akrongkofi@gmail.com>
2022-09-21 15:41:58 -0700

    Task two of more pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+# include "main.h"
+# include <string.h>
+
+/**
+ * _strncat -> function to append some char
+ * @dest: first param
+ * @src: second param
+ * @n : 3rd parm
+ * Return: string
+ *
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	strncat(dest, src, n);
+	return (dest);
+
+}
+
