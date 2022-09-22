fe03f0f202241df39e526bf2e54a35e5b8717aa2
Teddy10000 <akrongkofi@gmail.com>
2022-09-22 15:49:54 -0700

    another worked on

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,31 +15,20 @@ void print_number(int n)
 
 {
 
-	int divisor = 1, i, resp;
-
-
+	unsigned int num = n;
 
 	if (n < 0)
-
-	{
-
+		{
 		_putchar('-');
+		num = -num;
 
-		n *= -1;
-	}
+		}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	_putchar((num % 10) + '0');
 
-		;
 
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
 
-	{
-
-		resp = n / divisor;
-
-		_putchar('0' + resp);
-
-	}
 
 }
