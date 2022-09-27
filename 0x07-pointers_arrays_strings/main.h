c8bcd81bf79721d24f42d98211ebf8310d748a7a
Teddy10000 <akrongkofi@gmail.com>
2022-09-26 18:28:29 -0700

    Fixed

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,15 @@
 
+
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
 
+
+
 int _putchar(char c);
+
 void *_memset(void *s, int c, size_t n);
+
 void *_memcpy(void *dest, const void *src, size_t n);
 
 char *_strchr(char *s, char c);
