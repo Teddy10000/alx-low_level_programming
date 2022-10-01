a6b5b6411e32ae436fda6ee655ec2b100fa83e3a
Teddy10000 <akrongkofi@gmail.com>
2022-09-30 18:50:23 -0700

    This application just prints the number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -> This function prints the number of command line argument passed
+*@argc : takes in the number of command line argument.
+*@argv: contains the string of the list command line argument
+*  Return: 0
+*
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int a;
+	a = argc -1 ;
+	printf("%d\n", a);
+
+	return (0);
+}
+
